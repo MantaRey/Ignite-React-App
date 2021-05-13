ebc34f2faf479ef4d2dd0eb80d7b3c031ba4cc86
MantaRey <50933762+MantaRey@users.noreply.github.com>
2021-05-12 19:48:47 -0700

    Note Added to metaDataReducer.js
    Added a little Heading to explain why this State is separate from the others.

--- src/reducers/metaDataReducer.js
+++ src/reducers/metaDataReducer.js
@@ -1,3 +1,14 @@
+/*
+The Reason Why This Is Separate:
+
+"There's nothing wrong with your implementation of useEffect and useDispatch based off what you've provided.
+Whatever is causing your unintentional refreshes must be caused by a component higher up in your application that is being affected by the redux state changes.
+When the wrapping component subscribed to the redux state experiences a state change, it will re-render. This could cause a change in which components are mounted/unmounted.
+In your case, something is causing the Router to be remounted, which means that all the components in the routes with a useEffect hook will run those mounting effects again."
+
+Used to exist in the gamesReducer.js but changing a State (e.g. popular games list changes due to filter) would affect entire Home Page and cause a Reload
+*/
+
 const initState = {
   isLoading: true,
 };
